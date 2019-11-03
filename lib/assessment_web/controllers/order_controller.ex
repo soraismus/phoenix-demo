b021eb7f6dfad20b64ce59635ebf1b2a237e14e5
matthew.hilty <hilty.matthew@gmail.com>
2019-11-03 16:18:14 -0500

    snapshot

--- lib/assessment_web/controllers/order_controller.ex
+++ lib/assessment_web/controllers/order_controller.ex
@@ -4,9 +4,6 @@ defmodule AssessmentWeb.OrderController do
   alias Assessment.Orders
   alias Assessment.Orders.Order
 
-  @ok :ok
-  @error :error
-
   plug :authorize_order_management
 
   def index(conn, _params) do
@@ -21,44 +18,44 @@ defmodule AssessmentWeb.OrderController do
 
   def create(conn, %{"order" => order_params}) do
     case Orders.create_order(order_params) do
-      {@ok, order} ->
+      {:ok, order} ->
         conn
         |> put_flash(:info, "Order created successfully.")
         |> redirect(to: order_path(conn, :show, order))
-      {@error, %Ecto.Changeset{} = changeset} ->
+      {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
   end
 
   def show(conn, %{"id" => id}) do
-    with {@ok, order} <- Orders.get_order(id) do
+    with {:ok, order} <- Orders.get_order(id) do
       render(conn, "show.html", order: order)
     end
   end
 
   def edit(conn, %{"id" => id}) do
-    with {@ok, order} <- Orders.get_order(id) do
+    with {:ok, order} <- Orders.get_order(id) do
       changeset = Orders.change_order(order)
       render(conn, "edit.html", order: order, changeset: changeset)
     end
   end
 
   def update(conn, %{"id" => id, "order" => order_params}) do
-    with {@ok, order} <- Orders.get_order(id) do
+    with {:ok, order} <- Orders.get_order(id) do
       case Orders.update_order(order, order_params) do
-        {@ok, order} ->
+        {:ok, order} ->
           conn
           |> put_flash(:info, "Order updated successfully.")
           |> redirect(to: order_path(conn, :show, order))
-        {@error, %Ecto.Changeset{} = changeset} ->
+        {:error, %Ecto.Changeset{} = changeset} ->
           render(conn, "edit.html", order: order, changeset: changeset)
       end
     end
   end
 
   def delete(conn, %{"id" => id}) do
-    with {@ok, order} <- Orders.get_order(id),
-         {@ok, _order} = Orders.delete_order(order) do
+    with {:ok, order} <- Orders.get_order(id),
+         {:ok, _order} = Orders.delete_order(order) do
       conn
       |> put_flash(:info, "Order deleted successfully.")
       |> redirect(to: order_path(conn, :index))
