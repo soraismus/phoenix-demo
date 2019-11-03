58d9fef4de4a57d3dd8e678c81f9730ec7fd2e1d
matthew.hilty <hilty.matthew@gmail.com>
2019-11-03 01:43:26 -0500

    snapshot

--- lib/assessment/order_states/order_state.ex
+++ lib/assessment/order_states/order_state.ex
@@ -4,6 +4,8 @@ defmodule Assessment.OrderStates.OrderState do
   alias Assessment.Orders.Order
 
 
+  @order_states ~w(active canceled delivered undeliverable)s
+
   schema "order_states" do
     field :description, :string
     has_many :orders, Order
@@ -16,6 +18,21 @@ defmodule Assessment.OrderStates.OrderState do
     order_state
     |> cast(attrs, [:description])
     |> validate_required([:description])
+    |> validate_order_state()
     |> unique_constraint(:description)
+  end
+
+  defp validate_order_state(changeset) do
+    validate_change(changeset, :description, fn (:description, description) ->
+        if description in @order_states do
+          []
+        else
+          [description: "Must be one of 'active', 'canceled', 'delivered', or 'undeliverable'"]
+        end
+      end)
+  end
+
+  def order_states() do
+    @order_states
   end
 end
--- lib/assessment/orders/order.ex
+++ lib/assessment/orders/order.ex
@@ -14,6 +14,7 @@ defmodule Assessment.Orders.Order do
     belongs_to :pharmacy, Pharmacy
     belongs_to :courier, Courier
     belongs_to :order_state, OrderState
+    field :order_state_description, :string, virtual: true
 
     timestamps()
   end
@@ -21,11 +22,35 @@ defmodule Assessment.Orders.Order do
   @doc false
   def changeset(order, attrs) do
     order
-    |> cast(attrs, @required_params)
+    |> cast(attrs, @required_params ++ [:order_state_description])
     |> validate_required(@required_params)
+    |> validate_order_state_description()
+    |> set_order_state_id()
     |> foreign_key_constraint(:patient_id)
     |> foreign_key_constraint(:pharmacy_id)
     |> foreign_key_constraint(:courier_id)
     |> foreign_key_constraint(:order_state_id)
+  end
+
+  @order_state_descriptions ~w(active canceled delivered undeliverable)s
+  defp validate_order_state_description(changeset) do
+    validate_change(changeset, :order_state_description, fn (:order_state_description, description) ->
+        IO.puts("description is #{description}")
+        if description in @order_state_descriptions do
+          []
+        else
+          [order_state_description: "Must be one of 'active', 'canceled', 'delivered', or 'undeliverable'"]
+        end
+      end)
+  end
+
+  defp set_order_state_id(changeset) do
+    case get_change(changeset, :order_state_description) do
+      "active" -> put_change(changeset, :order_state_id, 1)
+      "canceled" -> put_change(changeset, :order_state_id, 2)
+      "delivered" -> put_change(changeset, :order_state_id, 3)
+      "undeliverable" -> put_change(changeset, :order_state_id, 4)
+      _ -> changeset
+    end
   end
 end
