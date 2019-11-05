e659e6051d927eda9f8930ccdc11f8db7bab5738
matthew.hilty <hilty.matthew@gmail.com>
2019-11-05 00:06:27 -0500

    snapshot

--- lib/assessment_web/views/order_view.ex
+++ lib/assessment_web/views/order_view.ex
@@ -2,12 +2,25 @@ defmodule AssessmentWeb.OrderView do
   use AssessmentWeb, :view
   import Assessment.Utilities, only: [get_date_today: 0, nilify_error: 1]
 
+  def current_order_state(conn) do
+    case conn.params["order_state"] do
+      "all" -> "all"
+      _     -> "active"
+    end
+  end
+
+  def current_pickup_date(conn) do
+    case conn.params["pickup_date"] do
+      "all" -> "all"
+      _     -> "today"
+    end
+  end
+
   def format_time(%Time{} = time) do
     time |> Time.to_iso8601() |> String.slice(0..4)
   end
 
   def get_qualifier(conn, %{order_state_id: order_state_id, pickup_date: pickup_date} = params) do
-    IO.inspect(params)
     count =
       case conn.assigns.agent.account_type do
         "courier" -> 4
@@ -28,4 +41,32 @@ defmodule AssessmentWeb.OrderView do
   end
 
   def get_default_time(), do: {13, 0, 0}
+
+  def next_order_state(conn) do
+    case conn.params["order_state"] do
+      "all" -> "active"
+      _     -> "all"
+    end
+  end
+
+  def next_pickup_date(conn) do
+    case conn.params["pickup_date"] do
+      "all" -> "today"
+      _     -> "all"
+    end
+  end
+
+  def order_state_label(conn) do
+    case conn.params["order_state"] do
+      "all" -> "Active"
+      _     -> "All Order States"
+    end
+  end
+
+  def pickup_date_label(conn) do
+    case conn.params["pickup_date"] do
+      "all" -> "Today"
+      _     -> "All Dates"
+    end
+  end
 end
