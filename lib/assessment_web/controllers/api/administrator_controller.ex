1b313034de158cfb857acdeeef5cc9846269d959
matthew.hilty <hilty.matthew@gmail.com>
2019-11-05 15:01:48 -0500

    snapshot

--- lib/assessment_web/controllers/api/administrator_controller.ex
+++ lib/assessment_web/controllers/api/administrator_controller.ex
@@ -3,32 +3,27 @@ defmodule AssessmentWeb.Api.AdministratorController do
   alias Assessment.Accounts
   alias Assessment.Accounts.Agent
 
+  action_fallback(AssessmentWeb.Api.ErrorController)
+
   def create(conn, %{"administrator" => %{"username" => u, "password" => p, "email" => e}}) do
     agent_params =
       %{}
       |> Map.put("username", u)
       |> Map.put("credential", %{"password" => p})
       |> Map.put("administrator", %{"email" => e})
-    case Accounts.create_administrator(agent_params) do
-      {:ok, %Agent{administrator: administrator}} ->
-        conn
-        |> render("create.json", administrator: administrator)
-      {:error, %Ecto.Changeset{} = changeset} ->
-        conn
-        |> json("Error")
+    with {:ok, agent} <- Accounts.create_administrator(agent_params) do
+      render(conn, "create.json", administrator: agent.administrator)
     end
   end
 
   def index(conn, _params) do
     administrators = Accounts.list_administrators()
-    conn
-    |> render("index.json", administrators: administrators)
+    render(conn, "index.json", administrators: administrators)
   end
 
   def show(conn, %{"id" => id}) do
     with {:ok, administrator} <- Accounts.get_administrator(id) do
-      conn
-      |> render("show.json", administrator: administrator)
+      render(conn, "show.json", administrator: administrator)
     end
   end
 end
---
+++ lib/assessment_web/controllers/api/error_controller.ex
@@ -0,0 +1,36 @@
+defmodule AssessmentWeb.Api.ErrorController do
+  use AssessmentWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    json(conn, %{errors: translate_errors(changeset)})
+  end
+
+  defp translate_error({msg, opts}) do
+    # When using gettext, we typically pass the strings we want
+    # to translate as a static argument:
+    #
+    #     # Translate "is invalid" in the "errors" domain
+    #     dgettext "errors", "is invalid"
+    #
+    #     # Translate the number of files with plural rules
+    #     dngettext "errors", "1 file", "%{count} files", count
+    #
+    # Because the error messages we show in our forms and APIs
+    # are defined inside Ecto, we need to translate them dynamically.
+    # This requires us to call the Gettext module passing our gettext
+    # backend as first argument.
+    #
+    # Note we use the "errors" domain, which means translations
+    # should be written to the errors.po file. The :count option is
+    # set by Ecto and indicates we should also apply plural rules.
+    if count = opts[:count] do
+      Gettext.dngettext(AssessmentWeb.Gettext, "errors", msg, msg, count, opts)
+    else
+      Gettext.dgettext(AssessmentWeb.Gettext, "errors", msg, opts)
+    end
+  end
+
+  defp translate_errors(changeset) do
+    Ecto.Changeset.traverse_errors(changeset, &translate_error/1)
+  end
+end
