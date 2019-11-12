124997f4d2361f1b25c288fe8ad7ec1f457cb446
matthew.hilty <hilty.matthew@gmail.com>
2019-11-12 15:14:28 -0500

    snapshot

--- test/assessment_web/controllers/browser/patient_controller_test.exs
+++ test/assessment_web/controllers/browser/patient_controller_test.exs
@@ -1,11 +1,12 @@
 defmodule AssessmentWeb.Browser.PatientControllerTest do
   use AssessmentWeb.ConnCase
 
+  import AssessmentWeb.Browser.ConnCase, only: [log_in_admin: 1]
+
   alias Assessment.Patients
 
-  @create_attrs %{address: "some address", name: "some name"}
-  @update_attrs %{address: "some updated address", name: "some updated name"}
   @invalid_attrs %{address: nil, name: nil}
+  @create_attrs %{name: "some name", address: "some address"}
 
   def fixture(:patient) do
     {:ok, patient} = Patients.create_patient(@create_attrs)
@@ -13,6 +14,8 @@ defmodule AssessmentWeb.Browser.PatientControllerTest do
   end
 
   describe "index" do
+    setup [:log_in_admin]
+
     test "lists all patients", %{conn: conn} do
       conn = get conn, patient_path(conn, :index)
       assert html_response(conn, 200) =~ "Listing Patients"
@@ -20,6 +23,8 @@ defmodule AssessmentWeb.Browser.PatientControllerTest do
   end
 
   describe "new patient" do
+    setup [:log_in_admin]
+
     test "renders form", %{conn: conn} do
       conn = get conn, patient_path(conn, :new)
       assert html_response(conn, 200) =~ "New Patient"
@@ -27,57 +32,32 @@ defmodule AssessmentWeb.Browser.PatientControllerTest do
   end
 
   describe "create patient" do
+    setup [:log_in_admin]
+
     test "redirects to show when data is valid", %{conn: conn} do
-      conn = post conn, patient_path(conn, :create), patient: @create_attrs
-
-      assert %{id: id} = redirected_params(conn)
-      assert redirected_to(conn) == patient_path(conn, :show, id)
-
-      conn = get conn, patient_path(conn, :show, id)
-      assert html_response(conn, 200) =~ "Show Patient"
+      response0 = post conn, patient_path(conn, :create), patient: @create_attrs
+      assert %{id: id} = redirected_params(response0)
+      assert redirected_to(response0) == patient_path(response0, :show, id)
+      response1 = get conn, patient_path(conn, :show, id)
+      assert html_response(response1, 200) =~ "Show Patient"
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      conn = post conn, patient_path(conn, :create), patient: @invalid_attrs
-      assert html_response(conn, 200) =~ "New Patient"
-    end
-  end
-
-  describe "edit patient" do
-    setup [:create_patient]
-
-    test "renders form for editing chosen patient", %{conn: conn, patient: patient} do
-      conn = get conn, patient_path(conn, :edit, patient)
-      assert html_response(conn, 200) =~ "Edit Patient"
-    end
-  end
-
-  describe "update patient" do
-    setup [:create_patient]
-
-    test "redirects when data is valid", %{conn: conn, patient: patient} do
-      conn = put conn, patient_path(conn, :update, patient), patient: @update_attrs
-      assert redirected_to(conn) == patient_path(conn, :show, patient)
-
-      conn = get conn, patient_path(conn, :show, patient)
-      assert html_response(conn, 200) =~ "some updated address"
-    end
-
-    test "renders errors when data is invalid", %{conn: conn, patient: patient} do
-      conn = put conn, patient_path(conn, :update, patient), patient: @invalid_attrs
-      assert html_response(conn, 200) =~ "Edit Patient"
+      response = post conn, patient_path(conn, :create), patient: @invalid_attrs
+      assert html_response(response, 200) =~ "New Patient"
     end
   end
 
   describe "delete patient" do
-    setup [:create_patient]
+    setup [:log_in_admin, :create_patient]
 
     test "deletes chosen patient", %{conn: conn, patient: patient} do
-      conn = delete conn, patient_path(conn, :delete, patient)
-      assert redirected_to(conn) == patient_path(conn, :index)
-      assert_error_sent 404, fn ->
-        get conn, patient_path(conn, :show, patient)
-      end
+      response0 = delete conn, patient_path(conn, :delete, patient)
+      assert redirected_to(response0) == patient_path(response0, :index)
+      response1 = get conn, patient_path(conn, :show, patient)
+      assert redirected_to(response1) == page_path(response1, :index)
+      error = "Patient ##{patient.id} does not exist"
+      assert get_flash(response1, :error) =~ error
     end
   end
 
