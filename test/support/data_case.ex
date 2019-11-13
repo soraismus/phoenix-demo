a680ab9b51b66cd444b9d14137e3947c0342247c
matthew.hilty <hilty.matthew@gmail.com>
2019-11-13 01:03:49 -0500

    snapshot

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -1,5 +1,39 @@
 defmodule Assessment.DataCase do
-  alias Assessment.{Accounts,Orders,Patients}
+  @moduledoc """
+  This module defines the setup for tests requiring
+  access to the application's data layer.
+
+  You may define functions here to be used as helpers in
+  your tests.
+
+  Finally, if the test case interacts with the database,
+  it cannot be async. For this reason, every test runs
+  inside a transaction which is reset at the beginning
+  of the test unless the test case is marked as async.
+  """
+
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      alias Assessment.Repo
+
+      import Ecto
+      import Ecto.Changeset
+      import Ecto.Query
+      import Assessment.DataCase
+    end
+  end
+
+  setup tags do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Assessment.Repo)
+
+    unless tags[:async] do
+      Ecto.Adapters.SQL.Sandbox.mode(Assessment.Repo, {:shared, self()})
+    end
+
+    :ok
+  end
 
   @doc """
   A helper that transform changeset errors to a map of messages.
@@ -16,6 +50,8 @@ defmodule Assessment.DataCase do
       end)
     end)
   end
+
+  alias Assessment.{Accounts,Orders,Patients}
 
   @administrator_attrs %{ username: "some username",
                           administrator: %{
