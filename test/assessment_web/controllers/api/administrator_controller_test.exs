62830682ac774fa0ada4b669f1b27a57afe839b8
matthew.hilty <hilty.matthew@gmail.com>
2019-11-13 15:20:28 -0500

    snapshot

--- test/assessment_web/controllers/api/administrator_controller_test.exs
+++ test/assessment_web/controllers/api/administrator_controller_test.exs
@@ -1,21 +1,11 @@
 defmodule AssessmentWeb.Api.AdministratorControllerTest do
   use AssessmentWeb.Api.ConnCase
 
-  import Assessment.DataCase, only: [fixture: 1, json_equiv?: 2]
+  import Assessment.DataCase, only: [json_equiv?: 2]
   import AssessmentWeb.Api.ConnCase,
     only: [ add_administrator: 1,
             log_in_admin: 1,
           ]
-
-  alias Assessment.Accounts.Administrator
-
-  @invalid_attrs %{email: nil}
-  @create_attrs %{ username: "some username",
-                   administrator: %{
-                     email: "some email",
-                   },
-                   credential: %{password: "some password"}
-                 }
 
   describe "index" do
     setup [:add_administrator, :add_administrator, :add_administrator, :log_in_admin]
@@ -27,38 +17,41 @@ defmodule AssessmentWeb.Api.AdministratorControllerTest do
     end
   end
 
-#  describe "create administrator" do
-#    setup [:log_in_admin]
-#
-#    test "redirects to show when data is valid", %{conn: conn} do
-#      response0 = post conn, api_administrator_path(conn, :create), agent: @create_attrs
-#      assert %{id: id} = redirected_params(response0)
-#      assert redirected_to(response0) == api_administrator_path(response0, :show, id)
-#      response1 = get conn, api_administrator_path(conn, :show, id)
-#      assert json_response(response1, 200) =~ "Show Administrator"
-#    end
-#
-#    test "renders errors when data is invalid", %{conn: conn} do
-#      response = post conn, api_administrator_path(conn, :create), agent: @invalid_attrs
-#      assert json_response(response, 400) =~ "New Administrator"
-#    end
-#  end
-#
-#  describe "delete administrator" do
-#    setup [:log_in_admin, :create_administrator]
-#
-#    test "deletes chosen administrator", %{conn: conn, administrator: administrator} do
-#      response0 = delete conn, api_administrator_path(conn, :delete, administrator)
-#      assert redirected_to(response0) == api_administrator_path(response0, :index)
-#      response1 = get conn, api_administrator_path(conn, :show, administrator)
-#      assert redirected_to(response1) == page_path(response1, :index)
-#      error = "Administrator ##{administrator.id} does not exist"
-#      assert get_flash(response1, :error) =~ error
-#    end
-#  end
-#
-#  defp create_administrator(_) do
-#    administrator = fixture(:administrator)
-#    {:ok, administrator: administrator}
-#  end
+  describe "show administrator" do
+    setup [:add_administrator, :log_in_admin]
+
+    test "creates and renders an administrator when the id is valid" , %{conn: conn, administrators: administrators} do
+      administrator = List.first(administrators)
+      response1 = get conn, api_administrator_path(conn, :show, administrator)
+      json = json_response(response1, 200)
+      assert json_equiv?(json["administrator"], administrator)
+    end
+  end
+
+  describe "create administrator" do
+    setup [:add_administrator, :log_in_admin]
+
+    test "creates and renders an administrator when the data is valid" , %{conn: conn} do
+      username = "some username"
+      email = "some email"
+      attrs = %{username: username, email: email, password: "some password"}
+
+      response0 = post conn, api_administrator_path(conn, :create), administrator: attrs
+      json = json_response(response0, :created)
+      created = json["created"]["administrator"]
+      created_id = created["id"]
+      template = %{"id" => created_id, "username" => username, "email" => email}
+      assert created == template
+
+      response1 = get conn, api_administrator_path(conn, :show, created_id)
+      json = json_response(response1, 200)
+      assert json["administrator"] == template
+    end
+
+    test "renders errors when data is invalid", %{conn: conn} do
+      invalid_attrs = %{}
+      response = post conn, api_administrator_path(conn, :create), administrator: invalid_attrs
+      json_response(response, 400)
+    end
+  end
 end
